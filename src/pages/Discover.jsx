296dc6baadb3a15031a447b587ec15ab5d8c82c1
Geronimo Viscontti <visconttig@gmail.com>
2022-11-05 22:50:21 -0300

    ...

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,36 +1,31 @@
-import {Error, Loader, SongCard} from "../components";
-import {genres} from "../assets/constants.js";
-
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants.js";
 
 const Discover = () => {
-console.log(genres);
-const genreTitle = "Pop";
+    const genreTitle = "Pop";
 
     return (
-        <div className="flex flex-col">
-            <div className="w-full flex justify-between 
-            items-center sm:flex-row flex-col mt-4 mb-10">
-                <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
-                <select
-                onChange={() => {}}
+        <div className="flex flex-col ">
+            <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+                <h2 className="text-white text-3xl font-bold text-left">Discover {genreTitle}</h2>
+                <select onChange={() => {}}
                 value=""
                 className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5">
-                    { genres.map((genre) => (
-                        <option key={genre.value} value={genre.value}>{genre.title}</option>
+                    {genres.map((genre, index) => (
+                        <option key={genre.value} value={genre.title}>{genre.title}</option>
                     ))}
                 </select>
             </div>
-            <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-                {[1,2,3, 4, 5, 6, 7, 8, 9, 10].map((song, index) => (
+            <div className="flex flex-wrap justify-start justify-center gap-8">
+                {[1,2,3,4,5,6,7,8,9,10].map((song, index) => (
                     <SongCard
-                    key={song.key}
+                    key={song.id}
                     song={song}
                     index={index} />
                 ))}
-                
             </div>
         </div>
-    )
+    );
 };
 
 export default Discover;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import playerReducer from './features/playerSlice';
-import { shazamCoreApi } from "./services/shazamCore.js";
 
+import playerReducer from './features/playerSlice';
 
 export const store = configureStore({
   reducer: {
-    [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
     player: playerReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(shazamCoreApi.middleware),
 });
