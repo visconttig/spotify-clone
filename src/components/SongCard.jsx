c308709877a855f5c56298a81cea97745f4016b7
Geronimo Viscontti <visconttig@gmail.com>
2022-11-06 04:09:40 -0300

    ...

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -9,14 +9,33 @@ const SongCard = ({ song, index }) => {
   // let activeSong.title = "test";
   let activeSong = "test";
 
+  const handlePlayClick = () => {
+
+  };
+
+  const handlePauseClick = () => {
+
+  };
+
 
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
     <div className="relative w-full h-56 group">
       <div className={`absolute inset-0 justify-center items-cetner bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? "flex bg-black bg-opacity-70" : "hidden" }`}>
-        <PlayPause />
+        <PlayPause 
+        song={song}
+        handlePlay={handlePlayClick}
+        handlePause={handlePauseClick}/>
       </div>
       <img alt="song_img" src={song?.images?.coverart} />
+    </div>
+    <div className="flex flex-col mt-4">
+      <p className="font-semibold text-lg text-white truncate">
+        <Link to={`/songs/${song?.key}`}>{song.title}</Link>
+      </p>
+      <p className="text-sm truncate text-gray-300 mt-1" >
+        <Link to={song.artists ? `/artits/${song?.artists[0]?.adamid}` : `/top-artists`} >{song.subtitle}</Link>
+      </p>
     </div>
   </div>
   );
