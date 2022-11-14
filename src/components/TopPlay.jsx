703169159e63bd1f5da2a520e93606d421fe157e
Geronimo Viscontti <visconttig@gmail.com>
2022-11-14 00:32:19 -0300

    TopPlay component initial setup.

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,38 @@
-const TopPlay = () => (
-  <div>TopPlay</div>
-);
+import { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+ import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper";
+import "swiper/css";
+import "swiper/css/free-mode";
+
+import PlayPause from "./PlayPause";
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+
+
+
+
+const TopPlay = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data } = useGetTopChartsQuery();
+// const divRef = useRef(null);
+
+
+
+  // useEffect(() => {
+  //   useRef(divRef).current.scrollIntoView({ behavior: "smooth"});
+  // });
+
+
+  return (
+    // ref
+    <div>
+
+    </div>
+  );
+}
 
 export default TopPlay;
