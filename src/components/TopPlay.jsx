e88c25d21faeaa2a156976e09afebc351f789f1d
Geronimo Viscontti <visconttig@gmail.com>
2022-11-14 01:08:47 -0300

    Create divs and add some styling.

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -18,21 +18,42 @@ const TopPlay = () => {
   const dispatch = useDispatch();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data } = useGetTopChartsQuery();
-// const divRef = useRef(null);
+const divRef = useRef(null);
+
+const topPlays = data?.slice(0, 5);
+
+useEffect(() => {
+  divRef.current.scrollIntoView({ behavior: "smooth"});
+});
+
+const handlePlayClick = () => {
+  dispatch(setActiveSong({ song, data, i }));
+  dispatch(playPause(true));
+};
+
+const handlePauseClick = () => {
+  dispatch(playPause(false));
+};
 
 
+return (
+  <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex felx-col">
+    <div className="w-full flex flex-col">
+      <div className="flex flex-row justify-between items-center">
+        <h2 className="text-white font-bold text-xl">Top Charts</h2>
+        <Link to="/top-charts">
+        <p className="text-gray-300 text-base cursor-pointer">See more</p>
+        </Link>
 
-  // useEffect(() => {
-  //   useRef(divRef).current.scrollIntoView({ behavior: "smooth"});
-  // });
-
-
-  return (
-    // ref
-    <div>
-
+      </div>
+      
     </div>
-  );
+
+  </div>
+)
+
+
+
 }
 
 export default TopPlay;
