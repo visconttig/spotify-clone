20f57161efc16baae4165484817119cb1a4c2c29
Geronimo Viscontti <visconttig@gmail.com>
2022-11-15 01:44:31 -0300

    Top Charts section and artists images.

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -13,9 +13,20 @@ import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const TopChartCard = ({song, i}) => (
   <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
-    { song.title }
+    <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
+    <div className="flex-1 flex flex-row justify-between items-center">
+        <img className="w-20 h-20 rounded-lg" src={song?.images?.coverart} alt={song?.title} />
+        <div className="flex flex-col flex-1 justify-center mx-3">
+          <Link to={`/songs/${song.key}`}>
+            <p className="text-xl font-bold text-white">{song?.title}</p>
+          </Link>
+          <Link to={`/songs/${song?.artists[0].adamid}`}>
+            <p className="text-base text-white">{song?.subtitle}</p>
+          </Link>
+        </div>
+    </div>
   </div>
-)
+);
 
 
 const TopPlay = () => {
@@ -76,21 +87,21 @@ return (
             >
               { topPlays?.map((song, i) => (
                 <SwiperSlide 
-                  key={song?.key}
-                  style={{ width: "25%", height: "auto"}}
-                  className="shadow-lg rounded-full animate-slideright"
-                >
-                  <Link to={`/artists/${song?.artists[0].adamid}`}>
-                    <img src={song?.images.background} alt="name" className="w-full rounded-full object-cover" />
-                  </Link>
-                </SwiperSlide>
-              ))}
+                key={song?.key}
+                style={{ width: "25%", height: "auto"}}
+                className="shadow-lg rounded-full animate-slideright"
+              >
+                <Link to={`/artists/${song?.artists[0].adamid}`}>
+                  <img src={song?.images.background} alt="name" className="w-full rounded-full object-cover" />
+                </Link>
+              </SwiperSlide>
+              ) )}
           </Swiper>
 
     </div>
 
   </div>
-)
+);
 
 
 
