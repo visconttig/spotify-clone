50ddddc73946385379afc4eafa5a78e903cd36a4
Geronimo Viscontti <visconttig@gmail.com>
2022-11-19 18:38:33 -0300

    Top artists component.

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -9,7 +9,8 @@ const RelatedSongs = ({ data, isPlaying, activeSong,
     <div className="mt-6 w-full flex flex-col">
       { data?.map((song, i) => (
         <SongBar 
-        key={`${song.key}-${artistId}`}
+        // passing index as key to avoid error due to duplicate song keys in external api
+        key={`${i}-${artistId}`}
         song={song}
         i={i} 
         artistId={artistId}
